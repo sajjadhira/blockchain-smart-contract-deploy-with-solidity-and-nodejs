48b6d8e55d255b2312c03c289c51186bf0c0a527
sajjadhira <sajjad.hira12@gmail.com>
2022-09-24 05:39:52 +0600

    getting contract data

--- test/message.js
+++ test/message.js
@@ -3,30 +3,31 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { abi, evm } = require("../compile");
 const secret = process.env.METAMASK_PHENOMENON;
-console.log(secret);
 
 const provider = new HDWalletProvider(
   secret,
   "https://goerli.infura.io/v3/6188e41b231048e4a6e0a5810dffb5ce"
 );
 
-let result;
 let contractAddress;
+let getMessage;
+let messageContent;
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account ", accounts[0]);
+  // console.log("Attempting to deploy from account ", accounts[0]);
 
-  result = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ["Hi etherum!"] })
-    .send({ gas: "1000000", from: accounts[0] });
-  contractAddress = result.options.address;
-  // console.log(result.options.address);
-
-  console.log(`contact deployed to ${contractAddress}`);
+  contractAddress = "0x10b94236603bbcaa21a6db1a4a955104c41338d0";
+  getMessage = await new web3.eth.Contract(abi, contractAddress).methods
+    .message()
+    .call()
+    .then((data) => {
+      messageContent = data;
+    });
+  console.log(messageContent);
 };
 
 deploy();
